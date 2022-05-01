bbb580c112fdf5ba499689ae302046861eda1d36
Daniel Schneider <daniel.schneider@eramux.com>
2022-05-01 10:21:32 +0200

    feat: fix engine and installation

--- src/vue-phosphor-icons.ts
+++ src/vue-phosphor-icons.ts
@@ -1,14 +1,12 @@
-// import { Plugin } from "vue";
+// Import vue components
+import * as components from './components/index'
 
-// // Import vue components
-// import * as components from './components/index'
-
-// export function install(app) {
-//   if (this.installed) return;
-//   this.installed = true;
-//   Object.entries(components).forEach(([componentName, component]) =>
-//     app.component(component.name, component)
-//   );
-// }
+export function install(app) {
+  if (this.installed) return;
+  this.installed = true;
+  Object.entries(components).forEach(([componentName, component]) =>
+    app.component(component.name, component)
+  );
+}
 
 export * from './components/index'
