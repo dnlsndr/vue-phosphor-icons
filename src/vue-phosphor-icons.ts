d4c2db29f6b0e863eb6786cf4233b5ac51828c79
Daniel Schneider <daniel.schneider@eramux.com>
2022-06-09 20:25:16 +0200

    feat: remove installed check

--- src/vue-phosphor-icons.ts
+++ src/vue-phosphor-icons.ts
@@ -2,8 +2,6 @@
 import * as components from './components/index'
 
 export function install(app) {
-  if (this.installed) return;
-  this.installed = true;
   Object.entries(components).forEach(([componentName, component]) =>
     app.component(component.name, component)
   );
