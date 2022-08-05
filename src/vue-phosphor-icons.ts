4aee138d4c7f6cfe12267edc7fc0c198f9c7a0bb
Daniel Schneider <daniel.schneider@eramux.com>
2022-08-05 08:36:47 +0200

    feat: remove install function

--- src/vue-phosphor-icons.ts
+++ src/vue-phosphor-icons.ts
@@ -1,10 +1,1 @@
-// Import vue components
-import * as components from './components/index'
-
-export function install(app) {
-  Object.entries(components).forEach(([componentName, component]) =>
-    app.component(component.name, component)
-  );
-}
-
 export * from './components/index'
